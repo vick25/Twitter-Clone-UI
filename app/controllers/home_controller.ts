aa736cfe8b9782a255e41544a44645e0e62bec75
Victor Kadiata <vickadiata@gmail.com>
2025-03-06 18:09:21 +0100

    Affichage des informations de profil statiques (avatar, nom d'utilisateur, bio, etc.)

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,14 +1,33 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 type Tweet = {
-    id: number,
-    content: string,
-    createdAt: Date,
+    id: number;
+    content: string;
+    createdAt: Date;
+    user?: number;
 }
 
-const tweets: Tweet[] = [
-    { id: 1, content: 'Developpeur fullstack from Kadea', createdAt: new Date() },
-    { id: 2, content: 'AdonisJS framework avec le Coach Sacré', createdAt: new Date() },
+export type User = {
+    id: number;
+    firstname: string;
+    lastname: string;
+    username: string;
+    avatar: string;
+    bio: string;
+    createdAt: Date;
+}
+
+export const tweets: Tweet[] = [
+    { id: 1, content: 'Developpeur fullstack from Kadea', createdAt: new Date(), user: 3 },
+    { id: 2, content: 'AdonisJS framework avec le Coach Sacré', createdAt: new Date(), user: 2 },
+    { id: 3, content: 'Apprendre COBOL pour gagner plus', createdAt: new Date(), user: 1 },
+]
+
+export const users: User[] = [
+    { id: 1, firstname: 'Victor', lastname: 'Kadiata', username: 'vick25', avatar: 'https://example.com/avatar1.jpg', bio: 'Le framework AdonisJS', createdAt: new Date() },
+    { id: 2, firstname: 'Sacré', lastname: 'Mbiku', username: 'coachsacre', avatar: 'https://example.com/avatar2.jpg', bio: 'Le Coach Sacré', createdAt: new Date() },
+    { id: 3, firstname: 'Alexis', lastname: 'Kabongo', username: 'kadea', avatar: 'https://example.com/avatar3.jpg', bio: 'Kadea', createdAt: new Date() },
+    { id: 4, firstname: 'Rosalie', lastname: 'Fayulu', username: 'adonisjs-community', avatar: 'https://example.com/avatar4.jpg', bio: 'Communauté AdonisJS', createdAt: new Date() },
 ]
 
 export default class HomeController {
--- app/controllers/profile_controller.ts
+++ app/controllers/profile_controller.ts
@@ -1,9 +1,16 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { User, users } from './home_controller.js';
 
 export default class ProfileController {
 
     async show({ params, view }: HttpContext) {
         const username = params?.username;
-        return view.render('pages/profile');
+        let user: User;
+        if (username)
+            user = users.find(u => u.username === username) as User
+        else
+            user = users[0]
+        console.log(user)
+        return view.render('pages/profile', { user });
     }
-}
+} 
