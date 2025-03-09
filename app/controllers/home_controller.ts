a307c5af3ac548308261f8e04c90c0cd739d1bb2
Victor Kadiata <vickadiata@gmail.com>
2025-03-09 23:11:50 +0100

    Refactoring et correction des styles pour ressembler à X

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,12 +1,17 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { tweets, users } from '../../utils/samples.js';
+import { tweets, User, users } from '../../utils/samples.js';
+
+function getUserFromTweet(userTweetID?: number) {
+    const userTweet = users.find(user => user.id === userTweetID);
+    return userTweet;
+}
 
 export default class HomeController {
 
     async index({ view }: HttpContext) {
         const userTweets = tweets.filter(tweet => tweet.user === users[0].id).reverse();
         const currentUser = users[0];
-        return view.render('pages/home', { userTweets, currentUser });
+        return view.render('pages/home', { tweets, currentUser, users });
     }
 
     async store({ request, response }: HttpContext) {
