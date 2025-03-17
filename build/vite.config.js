b13364eabb90382e9397af39ab476353a56efcdd
Victor Kadiata <vickadiata@gmail.com>
2025-03-17 18:19:02 +0100

    Public assets added to .gitignore and build manifest added to vite config

--- build/vite.config.js
+++ build/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
             reload: ['resources/views/**/*.edge'],
         }),
     ],
+    build: {
+        manifest: true
+    }
 });
 //# sourceMappingURL=vite.config.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,7 @@ export default defineConfig({
       reload: ['resources/views/**/*.edge'],
     }),
   ],
+  build: {
+    manifest: true
+  }
 })
