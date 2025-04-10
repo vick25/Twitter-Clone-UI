8861ac13735ab907d50521eaeb75da4fe7d35cb3
Victor Kadiata <vickadiata@gmail.com>
2025-04-10 13:47:34 +0100

    feat: implement post modal functionality with improved UI and event handling

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,17 @@ const openDialogButton = document.getElementById('openDialogButton');
 const jsCloseButton = document.getElementById('close');
 const jsClose = document.getElementById('close-dialog"');
 
+const modalPost = document.getElementById("formModalPost");
+const openModalPostBtn = document.getElementById("openModalPostBtn");
+const closeModalPostBtn = document.getElementById("closeModalPostBtn");
+
 document.addEventListener("DOMContentLoaded", (event) => {
+    window.addEventListener("click", (e) => {
+        if (e.target === modalPost) {
+            modalPost.classList.add("hidden");
+        }
+    });
+
     if (!logoutModal.classList.contains('hidden')) {
         logoutModal.classList.add('hidden');
     }
@@ -15,21 +25,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
         logoutModal.classList.toggle('hidden');
     });
 
-    openDialogButton.addEventListener('click', () => {
+    openDialogButton && openDialogButton.addEventListener('click', () => {
         dialog.showModal();
     });
 
     //Backdrop Close
-    dialog.addEventListener('click', (event) => {
-        if (event.target === dialog) {
+    dialog && dialog.addEventListener('click', (e) => {
+        if (e.target === dialog) {
             dialog.close();
         }
     });
 
-    jsCloseButton.addEventListener('click', function (e) {
+    jsCloseButton && jsCloseButton.addEventListener('click', function (e) {
         e.preventDefault();
         dialog.close();
     });
 
-    jsClose.dispatchEvent();
+    jsClose && jsClose.dispatchEvent();
+
+    // Listen for open click for the post modal
+    openModalPostBtn.addEventListener("click", () => {
+        modalPost.classList.remove("hidden");
+    });
+
+    closeModalPostBtn.addEventListener("click", () => {
+        modalPost.classList.add("hidden");
+    });
 });
