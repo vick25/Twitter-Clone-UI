ed4514268d38f65da5a7af5cbc37107ea6cf4c32
Victor Kadiata <vickadiata@gmail.com>
2025-04-10 16:27:48 +0100

    feat: enhance home page with retweet functionality and update like button styles

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -12,7 +12,8 @@ export default class HomeController {
     async index({ view }: HttpContext) {
 
         const tweets = await Tweet.query().orderBy('created_at', 'desc').preload('user')
-            .withCount('tweetLikes');
+            .withCount('tweetLikes')
+            .withCount('tweetRetweets');
         // const userTweets = tweets.filter(tweet => tweet.user === users[0].id).reverse();
         const currentUser = users[0];
         return view.render('pages/home', { tweets, currentUser, users });
