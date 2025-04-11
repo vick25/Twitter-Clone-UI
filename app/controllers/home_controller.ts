dff5ef08b7854f72b39b45fca62a8fa63a23fcb4
Victor Kadiata <vickadiata@gmail.com>
2025-04-11 13:26:04 +0100

    feat: simplify user display in tweet list by removing unnecessary user data references

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,22 +1,14 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { users } from '../../utils/samples.js';
 import Tweet from '#models/tweet';
-
-// function getUserFromTweet(userTweetID?: number) : User | undefined{
-//     const userTweet = users.find(user => user.id === userTweetID);
-//     return userTweet;
-// }
 
 export default class HomeController {
 
     async index({ view }: HttpContext) {
-
         const tweets = await Tweet.query().orderBy('created_at', 'desc').preload('user')
             .withCount('tweetLikes')
             .withCount('tweetRetweets')
             .withCount('tweetComments');
-        // const userTweets = tweets.filter(tweet => tweet.user === users[0].id).reverse();
-        const currentUser = users[0];
-        return view.render('pages/home', { tweets, currentUser, users });
+
+        return view.render('pages/home', { tweets });
     }
 }
