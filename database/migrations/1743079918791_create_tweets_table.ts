14d5fbf9752553947eff7b2876079f4641210a71
Victor Kadiata <vickadiata@gmail.com>
2025-04-11 15:55:14 +0100

    feat: add favicon and update image paths for profile and sidebar components

--- database/migrations/1743079918791_create_tweets_table.ts
+++ database/migrations/1743079918791_create_tweets_table.ts
@@ -8,7 +8,6 @@ export default class extends BaseSchema {
       table.increments('id')
       table.integer('user_id').unsigned().notNullable().references('users.id').onDelete('CASCADE')
       table.string('content', 280).notNullable()
-      table.integer('retweet_of').unsigned().nullable().references('tweets.id').onDelete('CASCADE')
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
